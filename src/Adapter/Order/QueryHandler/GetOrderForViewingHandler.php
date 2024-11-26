0bcb5d887228bf436e7f027a0405c5ab2d228f76
Hlavtox <daniel.hlavacek@hotmail.cz>
2024-11-26 14:28:44 +0100

    Handle no spent yet better

--- src/Adapter/Order/QueryHandler/GetOrderForViewingHandler.php
+++ src/Adapter/Order/QueryHandler/GetOrderForViewingHandler.php
@@ -243,7 +243,7 @@ final class GetOrderForViewingHandler extends AbstractOrderHandler implements Ge
         $currency = new Currency($order->id_currency);
         $customer = new Customer($order->id_customer);
         $genderName = '';
-        $totalSpentSinceRegistration = null;
+        $totalSpentSinceRegistration = 0;
 
         if (!Validate::isLoadedObject($customer)) {
             $customer = $this->buildFakeCustomerObject($order, $invoiceAddress);
@@ -290,7 +290,7 @@ final class GetOrderForViewingHandler extends AbstractOrderHandler implements Ge
             $genderName,
             $customer->email,
             new DateTimeImmutable($customer->date_add ?? 'now'),
-            $totalSpentSinceRegistration !== null ? $this->locale->formatPrice($totalSpentSinceRegistration, $currency->iso_code) : '',
+            $this->locale->formatPrice((float) $totalSpentSinceRegistration, $currency->iso_code),
             $customerStats['nb_orders'],
             $customer->note,
             (bool) $customer->is_guest,
