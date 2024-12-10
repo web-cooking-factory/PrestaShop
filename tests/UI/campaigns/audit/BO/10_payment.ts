db2a45c99d0fd499fdc07acf1efce5403666565f
Florine Hea <heaflorine@gmail.com>
2024-12-10 10:13:07 +0100

    fix lint

--- tests/UI/campaigns/audit/BO/10_payment.ts
+++ tests/UI/campaigns/audit/BO/10_payment.ts
@@ -14,64 +14,63 @@ import {
 const baseContext: string = 'audit_BO_payment';
 
 describe('BO - Payment', async () => {
-    let browserContext: BrowserContext;
-    let page: Page;
-  
-    before(async function () {
-      utilsPlaywright.setErrorsCaptured(true);
-  
-      browserContext = await utilsPlaywright.createBrowserContext(this.browser);
-      page = await utilsPlaywright.newTab(browserContext);
-    });
+  let browserContext: BrowserContext;
+  let page: Page;
 
-    after(async () => {
-        await utilsPlaywright.closeBrowserContext(browserContext);
-    });
+  before(async function () {
+    utilsPlaywright.setErrorsCaptured(true);
 
-    it('should login in BO', async function () {
-        await testContext.addContextItem(this, 'testIdentifier', 'loginBO', baseContext);
-    
-        await boLoginPage.goTo(page, global.BO.URL);
-        await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
-    
-        const pageTitle = await boDashboardPage.getPageTitle(page);
-        expect(pageTitle).to.contains(boDashboardPage.pageTitle);
-    
-        const jsErrors = utilsPlaywright.getJsErrors();
-        expect(jsErrors.length).to.equals(0);
-      });
+    browserContext = await utilsPlaywright.createBrowserContext(this.browser);
+    page = await utilsPlaywright.newTab(browserContext);
+  });
 
-      it('should go to \'Payment > Payment Methods\' page', async function () {
-        await testContext.addContextItem(this, 'testIdentifier', 'goToPaymentMethodsPage', baseContext);
-    
-        await boDashboardPage.goToSubMenu(
-          page,
-          boDashboardPage.paymentParentLink,
-          boDashboardPage.paymentMethodsLink,
-        );
-        await boPaymentMethodsPage.closeSfToolBar(page);
-    
-        const pageTitle = await boPaymentMethodsPage.getPageTitle(page);
-        expect(pageTitle).to.contains(boPaymentMethodsPage.pageTitle);
-    
-        const jsErrors = utilsPlaywright.getJsErrors();
-        expect(jsErrors.length).to.equals(0);
-      });
+  after(async () => {
+    await utilsPlaywright.closeBrowserContext(browserContext);
+  });
 
-      it('should go to \'Payment > Preferences\' page', async function () {
-        await testContext.addContextItem(this, 'testIdentifier', 'goToPaymentPreferencesPage', baseContext);
-    
-        await boDashboardPage.goToSubMenu(
-          page,
-          boDashboardPage.paymentParentLink,
-          boDashboardPage.preferencesLink,
-        );
-    
-        const pageTitle = await preferencesPage.getPageTitle(page);
-        expect(pageTitle).to.contains(preferencesPage.pageTitle);
-    
-        const jsErrors = utilsPlaywright.getJsErrors();
-        expect(jsErrors.length).to.equals(0);
-      });
+  it('should login in BO', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'loginBO', baseContext);
+
+    await boLoginPage.goTo(page, global.BO.URL);
+    await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
+
+    const pageTitle = await boDashboardPage.getPageTitle(page);
+    expect(pageTitle).to.contains(boDashboardPage.pageTitle);
+
+    const jsErrors = utilsPlaywright.getJsErrors();
+    expect(jsErrors.length).to.equals(0);
+  });
+
+  it('should go to \'Payment > Payment Methods\' page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToPaymentMethodsPage', baseContext);
+
+    await boDashboardPage.goToSubMenu(
+      page,
+      boDashboardPage.paymentParentLink,
+      boDashboardPage.paymentMethodsLink,
+    );
+    await boPaymentMethodsPage.closeSfToolBar(page);
+
+    const pageTitle = await boPaymentMethodsPage.getPageTitle(page);
+    expect(pageTitle).to.contains(boPaymentMethodsPage.pageTitle);
+
+    const jsErrors = utilsPlaywright.getJsErrors();
+    expect(jsErrors.length).to.equals(0);
+  });
+
+  it('should go to \'Payment > Preferences\' page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToPaymentPreferencesPage', baseContext);
+
+    await boDashboardPage.goToSubMenu(
+      page,
+      boDashboardPage.paymentParentLink,
+      boDashboardPage.preferencesLink,
+    );
+
+    const pageTitle = await preferencesPage.getPageTitle(page);
+    expect(pageTitle).to.contains(preferencesPage.pageTitle);
+
+    const jsErrors = utilsPlaywright.getJsErrors();
+    expect(jsErrors.length).to.equals(0);
+  });
 });
-
